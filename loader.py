492f8552eea931438d8775bbe99397ce9c39111f
Anastasiya Semizorova <semizorova2205@mail.ru>
2022-11-18 02:04:09 +0700

    Добавлен функционал реализованный в предыдущих коммитах

--- loader.py
+++ loader.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 from abc import abstractmethod
 from data import Data
+from indexes import Indexes
+from parameter import Parameter
 
 
 class Loader:
@@ -35,6 +37,8 @@ class Client:
 
     def __init__(self):
         self.handlers = []
+        self.columns_indexes = ["Quantity", "Description"]
+        self.param = ["UnitPrice"]
 
     def add_handler(self, handler):
         self.handlers.append(handler)
@@ -42,15 +46,23 @@ class Client:
     def response(self, filepath):
         for handler in self.handlers:
             try:
-                Data(handler.read(filepath))
+                data = Data(handler.read(filepath))
             except FileNotFoundError:
                 pass
             except ValueError:
                 pass
+        self.index(data)
+        self.parameters(data)
+
+    def index(self, data):
+        _ = [Indexes(data, col).index() for col in self.columns_indexes]
+
+    def parameters(self, data):
+        Parameter(self.columns_indexes, self.param, data).create()
 
 
 if __name__ == "__main__":
     client = Client()
     client.add_handler(LoaderExcel())
     client.add_handler(LoaderCSV())
-    client.response("")
+    client.response("Online_Retail.xlsx")
